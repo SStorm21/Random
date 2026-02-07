4186abfb851df52888fcb900ae29fdafdfcaf974
Storm⛈️ <176233500+SStorm21@users.noreply.github.com>
2026-02-07 17:36:40 +0300

    Add main.cpp with basic variable declarations and output

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //vars
+    int age = 20;
+    char g = 'm';
+    string name = "storm";
+    bool right = true;
+    bool wrong = false;
+
+
+    cout<< "( + ) this is a test file"<<endl;
+    cout<<"am "<<age<<" years old (int var)\n";
+    cout<<"i am a "<<g<<"ale (char var)\n";
+    cout<<"my name is "<<name<<" (str value)"<<endl;
+    cout<<"zero value is a "<<right<<"\nwrong value is a "<<wrong<<endl;
+    
+    return 0;
+}
